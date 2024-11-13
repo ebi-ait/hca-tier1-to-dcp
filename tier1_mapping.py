56420b5dd1d15f366b3a2578720f66487747d14c
Arsenios Chatzigeorgiou <49944135+arschat@users.noreply.github.com>
2024-11-13 17:22:07 +0000

    Reformat dict

--- tier1_mapping.py
+++ tier1_mapping.py
@@ -14,8 +14,7 @@ tier1_to_dcp = {
     'protocol_url': 'library_preparation_protocol.protocol_core.protocols_io_doi',
     # 'institute': 'project.contributors.institute',
     # 'sample_collection_site': 'sample_collection_site',
-    # 'sample_collection_relative_time_point': 
-    #     'specimen_from_organism.biomaterial_core.timecourse.value',
+    # 'sample_collection_relative_time_point': 'specimen_from_organism.biomaterial_core.timecourse.value',
     'library_id': 'cell_suspension.biomaterial_core.biomaterial_id',
     'library_id_repository': 'cell_suspension.biomaterial_core.biomaterial_name',
     # 'author_batch_notes': 'cell_suspension.biomaterial_core.biomaterial_description',
@@ -25,15 +24,12 @@ tier1_to_dcp = {
     # 'sex_ontology_term_id': 'donor_organism.sex',
     'sample_collection_method': 'collection_protocol.method.text',
     # tissue_type
-    'sampled_site_condition': 'specimen_from_organism.diseases.text', 
-    # if is healthy PATO,
-    #     if adjacent PATO & adjacent disease_ontology_term_id
-    #     else disease_ontology_term_id
+    # 'sampled_site_condition': 'specimen_from_organism.diseases.text', # if is healthy PATO, if adjacent PATO & adjacent disease_ontology_term_id, else disease_ontology_term_id
     'tissue_ontology_term_id': 'specimen_from_organism.organ.ontology',
     'tissue_free_text': 'specimen_from_organism.organ_parts.text',
     'sample_preservation_method': 'specimen_from_organism.preservation_storage.storage_method',
     'suspension_type': 'library_preparation_protocol.nucleic_acid_source',
-    'cell_enrichment': 'enrichment_protocol.markers', # if CL ontology add CL label
+    # 'cell_enrichment': 'enrichment_protocol.markers', # if CL ontology add CL label
     'cell_viability_percentage': 'cell_suspension.cell_morphology.percent_cell_viability',
     'cell_number_loaded': 'cell_suspension.estimated_cell_count',
     'sample_collection_year': 'specimen_from_organism.collection_time',
@@ -45,7 +41,7 @@ tier1_to_dcp = {
     # is_primary_data
     'reference_genome': 'analysis_file.genome_assembly_version',
     'gene_annotation_version': 'analysis_protocol.gene_annotation_version',
-    'alignment_software': 'analysis_protocol.alignment_software',
+    # 'alignment_software': 'analysis_protocol.alignment_software',
     'intron_inclusion': 'analysis_protocol.intron_inclusion',
     # author_cell_type
     # cell_type_ontology_term_id
@@ -101,43 +97,37 @@ tier1_list = ['title', 'study_pi',
               'self_reported_ethnicity_ontology_term_id', 
               'development_stage_ontology_term_id']
 """
-Dictionary with fields that could potentially generate a meaninigful protocol name 
-like i.e. 10x_3_v2_protocol, biopsy_protocol etc.
+Dictionary with fields that could potentially generate a meaninigful protocol name like i.e. 10x_3_v2_protocol, biopsy_protocol etc.
 """
 protocol_ids = {
     'Collection protocol':
     {
         'tier1_key': 'sample_collection_method',
         'dcp_key': 'collection_protocol.method.text',
-        'dcp_fields': ['collection_protocol.method.text', 
-                       'collection_protocol.protocol_core.protocol_id']
+        'dcp_fields': ['collection_protocol.method.text', 'collection_protocol.protocol_core.protocol_id']
     },
     'Library preparation protocol':
     {
         'tier1_key': 'assay_ontology_term_id',
         'dcp_key': 'library_preparation_protocol.library_construction_method.ontology',
-        'dcp_fields': ['library_preparation_protocol.library_construction_method.ontology', 
-                       'library_preparation_protocol.protocol_core.protocol_id']
+        'dcp_fields': ['library_preparation_protocol.library_construction_method.ontology', 'library_preparation_protocol.protocol_core.protocol_id']
     },
     'Sequencing protocol':
     {
         'tier1_key': 'sequencing_platform',
         'dcp_key': 'sequencing_protocol.instrument_manufacturer_model.ontology',
-        'dcp_fields': ['sequencing_protocol.instrument_manufacturer_model.ontology', 
-                       'sequencing_protocol.protocol_core.protocol_id']
+        'dcp_fields': ['sequencing_protocol.instrument_manufacturer_model.ontology', 'sequencing_protocol.protocol_core.protocol_id']
     },
     'Analysis protocol':
     {
         'tier1_key': 'analysis_software',
         'dcp_key': 'analysis_protocol.alignment_software_version',
-        'dcp_fields': ['analysis_protocol.alignment_software_version', 
-                       'analysis_protocol.protocol_core.protocol_id']
+        'dcp_fields': ['analysis_protocol.alignment_software_version', 'analysis_protocol.protocol_core.protocol_id']
     },
     'Enrichment protocol':
     {
         'tier1_key': 'cell_enrichment',
         'dcp_key': 'enrichment_protocol.markers',
-        'dcp_fields': ['enrichment_protocol.markers', 
-                       'enrichment_protocol.protocol_core.protocol_id']
+        'dcp_fields': ['enrichment_protocol.markers', 'enrichment_protocol.protocol_core.protocol_id']
     }
 }
