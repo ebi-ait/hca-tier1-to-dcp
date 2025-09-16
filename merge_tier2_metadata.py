46fa1b105d766a68bccda97ff66cc8f87f392a48
Arsenios Chatzigeorgiou <49944135+arschat@users.noreply.github.com>
2025-09-16 15:30:15 +0100

    Refactor code

--- merge_tier2_metadata.py
+++ merge_tier2_metadata.py
@@ -29,21 +29,24 @@ def rename_tier2_columns(tier2_df, tier2_to_dcp):
     return tier2_df.rename(columns=tier2_to_dcp)
 
 def split_lung_dissociation(tier2_df, lung_digest_dict):
-    all_diss_fields = ['dissociation_protocol.digestion_solution', 'dissociation_protocol.digestion_temperature', 'dissociation_protocol.digestion_time', 'dissociation_protocol.digestion_time_unit']
+    if tier2_df['protocol_tissue_dissociation'].isna().all():
+        if not tier2_df['protocol_tissue_dissociation_free_text'].isna().all():
+            print(f"Need to update schema to include {tier2_df.loc[tier2_df['protocol_tissue_dissociation_free_text'].notna(), 'protocol_tissue_dissociation_free_text'].unique()} in enum.")
+        else:
+            print("Only NA values. Skipping tissue_dissociation conversion")
+        return tier2_df
+    all_diss_fields = set().union(*[lung_digest_dict[prot].keys() for prot in tier2_df['protocol_tissue_dissociation'].unique()])
     for diss in all_diss_fields:
         tier2_df[diss] = pd.NA
-    if not tier2_df['protocol_tissue_dissociation'].isna().all():
-        for i, row in tier2_df.iterrows():
-            prot = row['protocol_tissue_dissociation']
-            if pd.isna(prot):
-                continue
-            if prot not in lung_digest_dict:
-                raise ValueError(f"Digestion protocol {prot} not in pre-defined enum. Should be under `protocol_tissue_dissociation`")
-            for diss in all_diss_fields:
-                tier2_df.loc[i, diss] = lung_digest_dict[prot].get(diss, pd.NA)
-        del tier2_df['protocol_tissue_dissociation']
-    if not tier2_df['protocol_tissue_dissociation_free_text'].isna().all():
-        print(f"Need to update schema to include {tier2_df.loc[tier2_df['protocol_tissue_dissociation_free_text'].notna(), 'protocol_tissue_dissociation_free_text'].unique()} in enum.")
+    for i, row in tier2_df.iterrows():
+        prot = row['protocol_tissue_dissociation']
+        if pd.isna(prot):
+            continue
+        if prot not in lung_digest_dict:
+            raise ValueError(f"Digestion protocol {prot} not in pre-defined enum. Should be under `protocol_tissue_dissociation`")
+        for diss in all_diss_fields:
+            tier2_df.loc[i, diss] = lung_digest_dict[prot].get(diss, pd.NA)
+    del tier2_df['protocol_tissue_dissociation']
     return tier2_df
 
 def manual_fix(tier2_df):
@@ -81,6 +84,13 @@ def get_tab_id(tab_name):
         raise ValueError(f"Unknown tab name: {tab_name}")
     return f"{tab_name.replace(' ', '_').lower()}.{entity_type}_core.{entity_type}_id"
     
+
+def get_fields_per_tab(tier2_df):
+    fields_per_tab = defaultdict(list)
+    for col in tier2_df.columns:
+        fields_per_tab[get_entity_type(col)].append(col)
+    return fields_per_tab
+
 def check_tab_in_spreadsheet(tab_name, spreadsheet):
         if tab_name not in spreadsheet:
             raise ValueError(f"Tab {tab_name} from Tier 2 metadata not found in spreadsheet.")
@@ -88,9 +98,12 @@ def check_tab_in_spreadsheet(tab_name, spreadsheet):
 def field_is_id(field_name):
     return re.match(r'[\w_]+\.\w+_core\.\w+_id', field_name) is not None
 
+def tab_is_protocol(tab_name):
+    return re.match(r'.*protocol', tab_name) is not None
+
 def check_field_already_in_spreadsheet(field_name, tab_name, spreadsheet):
     if field_name in spreadsheet[tab_name].columns and not field_is_id(field_name):
-        print(f"Field {field_name} already exists in tab {tab_name} of the spreadsheet. It will be overwritten with Tier 2 data.")
+        print(f"Field {field_name} already exists in tab {tab_name} of the spreadsheet. It will be discarded and overwritten with Tier 2 data.")
         del spreadsheet[tab_name][field_name]
 
 def check_key_in_spreadsheet(key, tab_name, spreadsheet):
@@ -101,31 +114,48 @@ def check_matching_keys(tier2_df, wrangled_spreadsheet, tab_name, key):
     if not wrangled_spreadsheet[tab_name][key].isin(tier2_df[key]).any():
         raise ValueError(f"No matching keys found between Tier 2 metadata and wrangled spreadsheet for tab {tab_name} using key {key}.")
 
-def merge_tier2_with_dcp(tier2_df, wrangled_spreadsheet):
-    fields_per_tab = defaultdict(list)
-    for col in tier2_df.columns:
-        fields_per_tab[get_entity_type(col)].append(col)
+def merge_overlap(wrangled_tab, tier2_fields, field_list, key):
+    merged_df = pd.merge(
+            wrangled_tab,
+            tier2_fields.drop_duplicates(),
+            on=key, how='outer', suffixes=('', '_t2')
+        )
+    common_cols = set(wrangled_tab.columns) & set(field_list) - {key}
+    for col in common_cols:
+        merged_df[col] = merged_df[col].combine_first(merged_df[f"{col}_t2"])
+        merged_df = merged_df.drop([f"{col}_t2"], axis=1)
+    return merged_df
 
-    for tab_name, field_list in fields_per_tab.items():
-        # do checks
-        check_tab_in_spreadsheet(tab_name, wrangled_spreadsheet)
-        if len(field_list) == 1 and field_is_id(field_list[0]):
-            continue
-        # TODO until protocols is done, we add info manually
-        if tab_name.endswith('protocol'):
-            print(f"Skipping merge for protocol {tab_name}, not yet implemented.")
-            continue
-        for field in field_list:
-            check_field_already_in_spreadsheet(field, tab_name, wrangled_spreadsheet)
-        key = get_tab_id(tab_name)
-        check_key_in_spreadsheet(key, tab_name, wrangled_spreadsheet)
-        check_matching_keys(tier2_df, wrangled_spreadsheet, tab_name, key)
-        # perform merge
-        wrangled_spreadsheet[tab_name] = pd.merge(
+def merge_sheets(wrangled_spreadsheet, tier2_df, tab_name, field_list, key, is_protocol):
+    check_key_in_spreadsheet(key, tab_name, wrangled_spreadsheet)
+    if is_protocol:
+        return merge_overlap(
+            wrangled_spreadsheet[tab_name],
+            tier2_df[field_list], field_list,
+            key)
+    check_matching_keys(tier2_df, wrangled_spreadsheet, tab_name, key)
+    return pd.merge(
             wrangled_spreadsheet[tab_name],
             tier2_df[field_list],
             on=key, how='left'
         )
+
+def merge_tier2_with_dcp(tier2_df, wrangled_spreadsheet):
+    fields_per_tab = get_fields_per_tab(tier2_df)
+
+    for tab_name, field_list in fields_per_tab.items():
+        # do checks
+        is_protocol = tab_is_protocol(tab_name)
+        check_tab_in_spreadsheet(tab_name, wrangled_spreadsheet)
+        if len(field_list) == 1 and field_is_id(field_list[0]):
+            continue
+        # TODO add protocol to applied biomaterial as well
+        if not tab_is_protocol(tab_name):
+            for field in field_list:
+                check_field_already_in_spreadsheet(field, tab_name, wrangled_spreadsheet) 
+        key = get_tab_id(tab_name)
+        # perform merge
+        wrangled_spreadsheet[tab_name] = merge_sheets(wrangled_spreadsheet, tier2_df, tab_name, field_list, key, is_protocol)
     return wrangled_spreadsheet
 
 def main():
@@ -149,7 +179,7 @@ def main():
     with pd.ExcelWriter(os.path.join(output_path, output_filename), engine='openpyxl') as writer:
         for tab_name, df in merged_df.items():
             df.to_excel(writer, sheet_name=tab_name, index=False)
-    return merged_df
+    print(f"Tier 2 metadata has been added to {os.path.join(output_path, output_filename)}.")
 
 if __name__ == "__main__":
     main()
