4bf8f6c22688ae72a8231931ed68f84d0715e776
idazucchi <45209056+idazucchi@users.noreply.github.com>
2026-03-23 15:03:36 +0000

    download all assets for one dataset

--- helper_files/get_cxg.py
+++ helper_files/get_cxg.py
@@ -173,7 +173,6 @@ def get_h5ad_from_collection(collection_id, output_dir="h5ads", auto_download_al
             for asset in dataset['assets']:
                 # todo: restore filetype check asset['filetype'] == 'H5AD'
                 h5ad_url = asset['url']
-                break
             
             if h5ad_url:
                 output_file = f'{output_dir}/{collection_id}_{dataset_id}.h5ad'
