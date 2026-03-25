151251bb9e02b5a308080504681942de7aa392f2
idazucchi <ida.zucchi@studio.unibo.it>
2026-03-25 16:34:48 +0000

    download just non-h5ad assets

--- helper_files/get_cxg.py
+++ helper_files/get_cxg.py
@@ -172,7 +172,8 @@ def get_h5ad_from_collection(collection_id, output_dir="h5ads", auto_download_al
             h5ad_url = None
             for asset in dataset['assets']:
                 # todo: restore filetype check asset['filetype'] == 'H5AD'
-                h5ad_url = asset['url']
+                if asset['filetype'] != 'H5AD':
+                    h5ad_url = asset['url']
             
             if h5ad_url:
                 output_file = f'{output_dir}/{collection_id}_{dataset_id}.h5ad'
